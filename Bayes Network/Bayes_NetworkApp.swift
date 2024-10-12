69df6017a39d477764141b6aebbf073269377d00
Philipp Kathöfer <philippkathoefr@gmail.com>
2024-10-11 17:22:10 -0700

    Initial Commit

---
+++ Bayes Network/Bayes_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bayes_NetworkApp.swift
+//  Bayes Network
+//
+//  Created by Philipp Kathöfer on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Bayes_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bayes Network/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bayes Network
+//
+//  Created by Philipp Kathöfer on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
